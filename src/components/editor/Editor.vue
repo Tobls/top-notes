3e3469588d5d3c4405256eb2bc72e5cb2082cbb7
Tobias Stuckenberger <tobias.stuckenberger@gmail.com>
2023-03-07 12:28:19 +0100

    Removed superfluous imports

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -12,11 +12,7 @@
     import "codemirror/mode/gfm/gfm.js"
     import 'codemirror/addon/fold/foldcode.js';
     import 'codemirror/addon/fold/foldgutter.js';
-    import 'codemirror/addon/fold/brace-fold.js';
-    import 'codemirror/addon/fold/xml-fold.js';
-    import 'codemirror/addon/fold/indent-fold.js';
     import 'codemirror/addon/fold/markdown-fold.js';
-    import 'codemirror/addon/fold/comment-fold.js';
     import {onMounted, ref} from "vue";
 
     const props = defineProps({
