6c26bc82630a3735e709372b69a44424ac225920
Tobias Stuckenberger <tobias.stuckenberger@gmail.com>
2023-03-13 17:04:21 +0100

    improved responsive header design

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,15 +1,17 @@
 <template>
-    <div class="w-full h-12 flex flex-row bg-brown-medium space-x-2 pl-1 pr-2 rounded">
+    <div class="relative w-full h-24 sm:h-12 flex flex-row bg-brown-medium space-x-2 pl-1 pr-2 rounded">
         <div class="w-fit sm:w-96 h-full flex flex-row items-center">
             <AppIcon />
-            <span class="text-4xl text-white whitespace-nowrap hidden sm:block">Top-Notes</span>
+            <span class="text-4xl text-white whitespace-nowrap hidden sm:block">T-Notes</span>
         </div>
-        <div class="w-full h-full flex flex-row items-center space-x-1">
-            <SaveButton />
-            <DeleteButton />
-            <NoteTitle title="test" />
-            <ResetTasksButton />
-            <SettingsButton />
+        <div class="h-full absolute sm:relative left-2 right-2 sm:w-full top-0 !ml-0 sm:pr-2">
+            <div class="w-full max-w-full h-full items-center space-x-1 grid grid-cols-[min-content_min-content_1fr_min-content_min-content] grid-rows-2 sm:grid-rows-1">
+                <SaveButton class="ml-12 sm:ml-0"/>
+                <DeleteButton />
+                <NoteTitle title="test" class="col-span-5 row-start-2 sm:row-start-1 sm:col-span-1 sm:col-start-3 !ml-0" />
+                <ResetTasksButton class="col-start-4"/>
+                <SettingsButton class="col-start-5"/>
+            </div>
         </div>
     </div>
 </template>
